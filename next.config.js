4f6b1442c24224c9576234fe86e4ad9b509a3a8e
Duc Le <duc.le.kyber@gmail.com>
2025-03-17 13:15:36 +0700

    Test deploy debugging

--- next.config.js
+++ next.config.js
@@ -8,8 +8,6 @@ const nextConfig = {
     domains: ['placehold.co'],
   },
   swcMinify: true,
-  // The `src` directory is now the source directory
-  distDir: 'build',
 };
 
 module.exports = nextConfig;
