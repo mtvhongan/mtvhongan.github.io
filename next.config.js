b5f322bce27477dc8d6c012385ddaf9a76ffb4ad
Duc Le <duc.le.kyber@gmail.com>
2025-03-20 22:09:01 +0700

    Auto reload

--- next.config.js
+++ next.config.js
@@ -6,8 +6,24 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placehold.co'],
+    unoptimized: true,
   },
   swcMinify: true,
+  webpack: (config, { isServer, dev }) => {
+    // Enable fast refresh explicitly
+    if (dev && !isServer) {
+      // Add React Refresh webpack plugin
+      config.devtool = 'eval-source-map';
+    }
+    return config;
+  },
+  // Enable page reloading when server-side changes are made
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in memory
+    maxInactiveAge: 25 * 1000,
+    // Number of pages that should be kept in memory
+    pagesBufferLength: 4,
+  },
 };
 
 module.exports = nextConfig;
