a662ce7ce1de21191a187d1ba07339ae7a3925b7
Duc Le <duc.le.kyber@gmail.com>
2025-03-24 16:21:44 +0700

    Fix lint

--- src/app/agricultural-machinery-page/page.tsx
+++ src/app/agricultural-machinery-page/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 import { AgriculturalBenefits } from './AgriculturalBenefits';
-import AgriculturalMachineryLayout from './AgriculturalMachineryLayout';
+import { AgriculturalMachineryLayout } from './AgriculturalMachineryLayout';
 import { Footer } from '@/components/layout/Footer';
 import { Header } from '@/components/layout/Header';
 
