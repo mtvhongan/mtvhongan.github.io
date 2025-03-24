db20acc18270ffbebdb6747d45f1923f56b8ae9b
Duc Le <duc.le.kyber@gmail.com>
2025-03-24 14:24:32 +0700

    AutomationEquipmentLayout wip

---
+++ src/app/automation-equipment-page/AutomationEquipmentLayout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+
+interface AutomationEquipmentLayoutProps {
+
+}
+
+export const AutomationEquipmentLayout: React.FC<AutomationEquipmentLayoutProps> = ({ children }) => {
+  return (
+    
+  );
+}
+
+export default AutomationEquipmentLayout;
