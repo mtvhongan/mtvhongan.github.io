31c044adcdb2ad993e61f6b6644ace111cc78b28
ldcduc <duc.le.kyber@gmail.com>
2025-03-24 12:37:56 +0000

    Deploying to gh-pages from @ mtvhongan/mtvhongan.github.io@1ffc06fe05c050287f3fe95d4818b5bacb7071ad 🚀

---
+++ _next/static/chunks/app/chinh-sach/bao-hanh/page-ed85013c07597cb0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[660,383,468,891,701,8,694,774],{2788:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,1131)),Promise.resolve().then(s.bind(s,7189))}},function(e){e.O(0,[726,557,971,117,744],function(){return e(e.s=2788)}),_N_E=e.O()}]);
